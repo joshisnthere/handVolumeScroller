4fa363f946c027f00fb4867fc8aa634ab7d2810d
Joshua Mutavi <alolagamerza@gmail.com>
2026-09-08 16:38:32 -0500

    Add core logic

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Hand Gesture Volume Control
+
+Pinch your thumb and index finger together or apart in front of the webcam
+to lower or raise your system volume. Hand tracking works anywhere;
+the actual volume control uses pycaw, which is Windows only.
+"""
