b551d519d377dd60b70eb76728f96483388e769c
Joshua Mutavi <alolagamerza@gmail.com>
2026-09-19 22:34:12 -0500

    Wire up system volume control

---
+++ volume_control.py
@@ -0,0 +1,5 @@
+"""
+Thin wrapper around pycaw so the rest of the app doesn't touch COM
+interfaces directly. get_volume()/set_volume() work on a 0.0-1.0 scale.
+Falls back to a no-op if pycaw isn't available (non-Windows systems).
+"""
